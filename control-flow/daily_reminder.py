fb531cc973f4e1c2b9ef801b46c139b579911190
yonasi <yonasma416@gmail.com>
2024-12-22 23:56:44 +0300

    daily reminder file modified

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,20 +1,22 @@
 task_discription = input("Enter your task: ")
-task_priority = input("Priority (high/medium/low): ")
+priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-match time_bound:
-    case "yes":
-        if task_priority.lower() == "high":
-            print(f"Reminder: '{task_discription}' is a {task_priority} priority task that requires immediate attention today!")
-        elif task_priority.lower() == "medium":
-            print(f"Reminder: '{task_discription}' is a {task_priority} priority task that requires your attention before the deadline!")
-        elif task_priority.lower() == "low":
-            print(f"Reminder: '{task_discription}' is a {task_priority} priority task that requires your attention before the deadline!")
+match priority:
+    case "high":
+         if time_bound == "yes":
+            print(f"Reminder: '{task_discription}' is a {priority} priority task that requires immediate attention today!")
+        elif time_bound == "no":
+            print(f"Note: '{task_discription}' is a {priority} priority task. Consider completing it when you have free time.")
 
-    case "no":
-        if task_priority.lower() == "high":
-            print(f"Note: '{task_discription}' is a {task_priority} priority task. Consider completing it when you have free time.")
-        elif task_priority.lower() == "medium":
-            print(f"Note: '{task_discription}' is a {task_priority} priority task. Consider completing it when you have free time.")
-        elif task_priority.lower() == "low":
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: '{task_discription}' is a {priority} priority task that requires your attention before the deadline!")
+        elif time_bound == "no":
+            print(f"Note: '{task_discription}' is a {priority} priority task. Consider completing it when you have free time.")
+
+    case "low":
+        if time_bound == "yes":
+            print(f"Reminder: '{task_discription}' is a {task_priority} priority task. Consider completing it before the deadline!")
+        elif time_bound == "no":
             print(f"Note: '{task_discription}' is a {task_priority} priority task. Consider completing it when you have free time.")
