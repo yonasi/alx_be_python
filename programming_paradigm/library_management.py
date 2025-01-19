9a59fccd528fb0ec9b7853ec5c88e54e474cdaa9
Yonas Mamo <63198859+yonasi@users.noreply.github.com>
2025-01-19 23:43:04 +0300

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,15 +3,12 @@ class Book:
         self.title = title
         self.author = author
         self._is_checked_out = False
+    def return_book(self):
+        self._is_checked_out = False
 
     def check_out(self):
         self._is_checked_out = True
 
-    def check_in(self):
-        self._is_checked_out = False
-
-    def is_available(self):
-        return not self._is_checked_out
 
 
 class Library:
