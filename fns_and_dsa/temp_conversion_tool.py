0110cc65dd3fd9e2c938461e34ae9e580f673c7e
yonasi <yonasma416@gmail.com>
2025-01-25 16:13:32 +0300

    local commit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,13 +1,13 @@
 temprature = int(input("Enter the temprature to convert: "))
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)*(temprature-32)
-CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)*(temprature + 32)
-unit = input("Is this temprature in Celcius or Fafarenheit? (C/F): ")
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9*temprature) - 32
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5*temprature) + 32
+unit = input("Is this temprature in Celcius or Fahrenheit? (C/F): ")
 def convert_to_celsius(fahrenheit):
-     temp_in_C = FAHRENHEIT_TO_CELSIUS_FACTOR
-     print(f"{temprature}F is {temp_in_C}C")
+     temp_in_C = FAHRENHEIT_TO_CELSIUS_FACTOR 
+     print(f"{temprature}°F is {temp_in_C}°C")
 def convert_to_fahrenheit(celsius):
     temp_in_F = CELSIUS_TO_FAHRENHEIT_FACTOR
-    print(f"{temprature}C is {temp_in_F}F ")
+    print(f"{temprature}°C is {temp_in_F}°F ")
 
 if unit.lower() == "c" and type(temprature) is int:
      convert_to_fahrenheit(temprature)
